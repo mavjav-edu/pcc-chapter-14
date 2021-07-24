11f0d8227e7cf55b61b73fcdb2595b38630aae10
Mavaddat Javid <mavaddat.javid@gmail.com>
2021-07-23 18:04:51 -0700

    update to pcc_2e

--- chapter_14/adding_play_button/button.py
+++ chapter_14/adding_play_button/button.py
@@ -1,11 +1,11 @@
 import pygame.font
-
-class Button():
-
-    def __init__(self, ai_settings, screen, msg):
+ 
+class Button:
+ 
+    def __init__(self, ai_game, msg):
         """Initialize button attributes."""
-        self.screen = screen
-        self.screen_rect = screen.get_rect()
+        self.screen = ai_game.screen
+        self.screen_rect = self.screen.get_rect()
         
         # Set the dimensions and properties of the button.
         self.width, self.height = 200, 50
@@ -13,21 +13,21 @@ class Button():
         self.text_color = (255, 255, 255)
         self.font = pygame.font.SysFont(None, 48)
         
-        # Build the button's rect object, and center it.
+        # Build the button's rect object and center it.
         self.rect = pygame.Rect(0, 0, self.width, self.height)
         self.rect.center = self.screen_rect.center
         
-        # The button message only needs to be prepped once.
-        self.prep_msg(msg)
+        # The button message needs to be prepped only once.
+        self._prep_msg(msg)
 
-    def prep_msg(self, msg):
-        """Turn msg into a rendered image, and center text on the button."""
+    def _prep_msg(self, msg):
+        """Turn msg into a rendered image and center text on the button."""
         self.msg_image = self.font.render(msg, True, self.text_color,
-            self.button_color)
+                self.button_color)
         self.msg_image_rect = self.msg_image.get_rect()
         self.msg_image_rect.center = self.rect.center
-        
+
     def draw_button(self):
-        # Draw blank button, then draw message.
+        # Draw blank button and then draw message.
         self.screen.fill(self.button_color, self.rect)
         self.screen.blit(self.msg_image, self.msg_image_rect)
